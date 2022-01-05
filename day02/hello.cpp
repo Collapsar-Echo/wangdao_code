9876b762c822199579bbd46d2af62dbb8b06cd86
i-turing <i_turing@126.com>
2022-01-05 12:14:59 +0800

    first cpp

---
+++ day02/hello.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: Wed 05 Jan 2022 11:58:34 AM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	printf("hello world!\n");
+	cout << "hello world!" << endl;
+
+	return 0;
+}
