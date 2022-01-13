2c05c94c9bb3852599ad84ca67e257c6edd4ce10
i-turing <i_turing@126.com>
2022-01-13 09:57:05 +0800

    ..

---
+++ day06/a.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: a.cpp
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: Thu 13 Jan 2022 09:54:09 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout << "wefrfjrewhfo";
+}
+
